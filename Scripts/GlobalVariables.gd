0e76171a3cdaa1751d6b94d16e434a2738948515
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-10-29 19:28:45 -0400

    Create GlobalVariables.gd

---
+++ Scripts/GlobalVariables.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const DEFAULT_HEALTH = 100
+const DEFAULT_MONEY = 200
+
+#0 = Normal, 1 = Biuld
+var mouseMode = 0
+
+var biuldingTemplates = []
+
+func setMouseMode(mode):
+	if mode == 0:
+		Input.set_custom_mouse_cursor(null)
+	elif mode == 1:
+		Input.set_custom_mouse_cursor(preload("res://Sprites/Icons/Mouse Cursors/TowerCreate.png"))
+	mouseMode = mode
