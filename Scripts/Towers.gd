c11f02b9293ee3aaf90885fb68791e01652e5b23
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-10-29 19:29:49 -0400

    Create Towers.gd

---
+++ Scripts/Towers.gd
@@ -0,0 +1,14 @@
+extends YSort
+
+var currentTemplate = null
+
+func placeTower(templateInt, _position):
+	var tower = preload("res://Characters/Tower.tscn")
+	var t = tower.instance()
+	add_child(t)
+	t.setData(GlobalVariables.biuldingTemplates[templateInt], _position)
+	
+	t = null
+	get_parent().get_node("ControlPanel/ColorRect/Stats").add_to_stat("money", -GlobalVariables.biuldingTemplates[templateInt][5])
+	GlobalVariables.setMouseMode(0)
+	get_parent().get_node("MouseAttachments")._hide("RangeCircle")
