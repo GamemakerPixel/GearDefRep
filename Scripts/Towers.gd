4d41c304513b6f6fa3029a69ffa92d688044d132
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-10-29 20:59:02 -0400

    Update Towers.gd

--- Scripts/Towers.gd
+++ Scripts/Towers.gd
@@ -12,3 +12,12 @@ func placeTower(templateInt, _position):
 	get_parent().get_node("ControlPanel/ColorRect/Stats").add_to_stat("money", -GlobalVariables.biuldingTemplates[templateInt][5])
 	GlobalVariables.setMouseMode(0)
 	get_parent().get_node("MouseAttachments")._hide("RangeCircle")
+
+func sellTower(_position):
+	var towers = get_children()
+	for tower in towers:
+		if tower.position == _position:
+			var worth = tower.template[5]
+			var sellValue = int(worth*0.75)
+			get_parent().get_node("ControlPanel/ColorRect/Stats").add_to_stat("money", sellValue)
+			tower.queue_free()
