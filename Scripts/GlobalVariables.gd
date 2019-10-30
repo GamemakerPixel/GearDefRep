ec803d015c03855a966c431db5e7c9a97e6cf2af
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-10-29 21:26:40 -0400

    Update GlobalVariables.gd

--- Scripts/GlobalVariables.gd
+++ Scripts/GlobalVariables.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var content = "TestContent"
+
 const DEFAULT_HEALTH = 100
 const DEFAULT_MONEY = 200
 
@@ -14,3 +16,15 @@ func setMouseMode(mode):
 	elif mode == 1:
 		Input.set_custom_mouse_cursor(preload("res://GearDefRep/Sprites/Icons/Mouse Cursors/TowerCreate.png"))
 	mouseMode = mode
+
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		savePlaytestData()
+
+func savePlaytestData():
+	print("Saving...")
+	var file = File.new()
+	print("File - " + str(file))
+	file.open("res://GearDefRep/PlaytesterData/playtest_data.dat", File.WRITE)
+	file.store_string(content)
+	file.close()
